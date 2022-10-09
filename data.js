e7dd94d9dbe6026c040d574d4ebb5d1ccc25161d
oxifo <113475475+oxifo@users.noreply.github.com>
2022-10-09 12:45:22 +0630

    Add files via upload

--- data.js
+++ data.js
@@ -1,14 +1,24 @@
 const data =[{
-         id:4,path:"zxcvb"},
+      id:5,
+      path:"dfddtf5",
+      title:"Set Your own way"
+     },{
+         id:4,
+         path:"zxcvb",
+         title:"Think it before to do"
+     },
      {
          id:1,
-         path:"letmedie"
+         path:"letmedie",
+         title:"Life is full with hurt"
      },{
          id:2,
-         path:"butme"
+         path:"butme",
+         title:"I have a little word for you"
      },
      {
          id:3,
-         path:"lanmharr"
+         path:"lanmharr",
+         title:"Are you kidden me"
      }
-     ]
+     ];
