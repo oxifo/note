23869b431fc4cf81d03e1b78e9bc22fd43201dac
oxifo <113475475+oxifo@users.noreply.github.com>
2022-10-12 03:49:08 +0630

    Add files via upload

--- data.js
+++ data.js
@@ -1,24 +1,31 @@
-const data =[{
-      id:5,
-      path:"dfddtf5",
-      title:"Set Your own way"
-     },{
-         id:4,
-         path:"zxcvb",
-         title:"Think it before to do"
-     },
-     {
-         id:1,
-         path:"letmedie",
-         title:"Life is full with hurt"
-     },{
-         id:2,
-         path:"butme",
-         title:"I have a little word for you"
-     },
-     {
-         id:3,
-         path:"lanmharr",
-         title:"set way"
-     }
-     ];
+const data = [{
+    id:7,
+    path:"computervirus",
+    title:"how to protest my computer away from the virus attack"
+}, {
+    id: 6,
+    path: "smartphoneupdate6",
+    title: "The advance of system update"
+}, {
+    id: 5,
+    path: "dfddtf5",
+    title: "Set Your own way"
+}, {
+    id: 4,
+    path: "zxcvb",
+    title: "Think it before to do"
+},
+    {
+        id: 1,
+        path: "letmedie",
+        title: "Life is full with hurt"
+    }, {
+        id: 2,
+        path: "butme",
+        title: "I have a little word for you"
+    },
+    {
+        id: 3,
+        path: "lanmharr",
+        title: "set way"
+    }];
